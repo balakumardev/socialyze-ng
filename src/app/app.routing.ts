4b6a5b1f069a85ae10b135245d8305fc5fcd2ae9
Bala Kumar <balakumarrulez@gmail.com>
2017-07-10 13:58:52 +0530

    Initial Commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//Layouts
+import { FullLayoutComponent } from './layouts/full-layout.component';
+import { SimpleLayoutComponent } from './layouts/simple-layout.component';
+import {ViewTweetsComponent} from "./view-tweets/view-tweets.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: FullLayoutComponent,
+    data: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: './dashboard/dashboard.module#DashboardModule'
+      },
+      {
+        path: 'view-tweets',
+        component: ViewTweetsComponent,
+        data: {
+          title: 'View Tweets'
+        }
+      }
+    ]
+  },
+  {
+    path: 'pages',
+    component: SimpleLayoutComponent,
+    data: {
+      title: 'Pages'
+    },
+    children: [
+      {
+        path: '',
+        loadChildren: './pages/pages.module#PagesModule',
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
---
+++ src/app/view-tweets/view-tweets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-tweets',
+  templateUrl: './view-tweets.component.html',
+  styleUrls: ['./view-tweets.component.scss']
+})
+export class ViewTweetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
