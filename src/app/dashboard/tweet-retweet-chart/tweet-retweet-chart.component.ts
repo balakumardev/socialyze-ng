b5ea0806345aab5cfe6d8ac6a632e3967e8e7ed6
anshulagarwal782 <anshulagarwal782@gmail.com>
2017-10-05 19:34:48 +0530

    no change

--- src/app/dashboard/tweet-retweet-chart/tweet-retweet-chart.component.ts
+++ src/app/dashboard/tweet-retweet-chart/tweet-retweet-chart.component.ts
@@ -1,5 +1,6 @@
 import {AfterContentInit, Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {DataService} from "../../data.service";
 @Component({
   selector: 'app-tweet-retweet-chart',
   templateUrl: './tweet-retweet-chart.component.html',
@@ -8,15 +9,22 @@ import {ActivatedRoute} from "@angular/router";
 export class TweetRetweetChartComponent implements OnInit, AfterContentInit {
   tweets : object;
   retweets : object;
+  hashtag : any;
+  resp  : any;
   loaded: boolean = false;
-  constructor(private thisroute: ActivatedRoute) { }
+  constructor(private thisroute: ActivatedRoute,private data: DataService) { }
 
   ngOnInit() {
 		this.tweets = this.thisroute.snapshot.queryParams['tweets'];
 		this.retweets = this.thisroute.snapshot.queryParams['retweets'];
+    this.hashtag = this.thisroute.snapshot.queryParams['hash'];
+    this.data.getSentimentScore(this.hashtag).subscribe(
+      (response) => {
+           this.resp = response.json();
+      });
   }
   // Pie
-  public pieChartLabels: string[] = ['Tweets', 'Retweets'];
+  public pieChartLabels: string[] = ['Positive', 'Negative','Neutral'];
   public pieChartData: number[];
   public pieChartType: string = 'pie';
 	public barChartOptions: any = {
@@ -32,7 +40,7 @@ export class TweetRetweetChartComponent implements OnInit, AfterContentInit {
   public doughnutChartData: number[];
   public doughnutChartType: string = 'doughnut';
   ngAfterContentInit() {
-    this.pieChartData = [+this.tweets, +this.retweets];
+    this.pieChartData = [+this.resp['positive'], +this.resp['negative'],+this.resp['neutral']];
 	 this.doughnutChartData = [+this.tweets, +this.retweets];
 	this.barChartData = [
     {data: [+this.tweets], label: 'tweets'},
@@ -41,7 +49,7 @@ export class TweetRetweetChartComponent implements OnInit, AfterContentInit {
     console.log(this.pieChartData);
     this.loaded = true;
   }
-  
+
   // events
   public chartClicked(e: any): void {
     console.log(e);
